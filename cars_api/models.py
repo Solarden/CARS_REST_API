06bcf7ef1493e711eb75ba1569d653255617d287
solarden <pchraczynski@gmail.com>
2021-12-28 20:53:59 +0100

    added models Car; CarRate

---
+++ cars_api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Car(models.Model):
+    make = models.CharField(max_length=64)
+    model = models.CharField(max_length=64, unique=True)
+
+
+RATING = ((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'))
+
+
+class CarRate(models.Model):
+    car_id = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True, blank=True)
+    rating = models.IntegerField(choices=RATING)
