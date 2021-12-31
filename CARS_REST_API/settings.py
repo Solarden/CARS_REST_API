eac92500fe01ced5027b57d89963ac8116d6ea3e
solarden <pchraczynski@gmail.com>
2021-12-31 13:03:49 +0100

    added heroku db connection

--- CARS_REST_API/settings.py
+++ CARS_REST_API/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,13 +76,16 @@ WSGI_APPLICATION = 'CARS_REST_API.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cars_rest_db',
-        'USER': 'cars_user',
-        'PASSWORD': 'api_pass',
-        'HOST': 'db',
-        'PORT': '5432'
+        'NAME': 'database name',
+        'USER': 'database user',
+        'PASSWORD': 'database password',
+        'HOST': 'database endpoint',
+        'PORT': 'database port'
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
