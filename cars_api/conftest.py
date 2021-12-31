a7e4e808083c42716dd7d3d35b2fcaca4542de09
solarden <pchraczynski@gmail.com>
2021-12-31 15:35:48 +0100

    updated tests

--- cars_api/conftest.py
+++ cars_api/conftest.py
@@ -5,23 +5,23 @@ from rest_framework.test import APIClient
 from cars_api import models
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def client():
     client = APIClient()
     return client
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def set_up_car():
     return models.Car.objects.create(make='Ford', model='Focus')
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def set_up_cars():
     models.Car.objects.create(make='Honda', model='Accord')
     models.Car.objects.create(make='Honda', model='Civic')
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def set_up_car_rate(set_up_car):
     return models.CarRate.objects.create(car_id=set_up_car, rating=randint(1, 5))
--- cars_api/tests.py
+++ cars_api/tests.py
@@ -1,18 +1,18 @@
 import pytest
-from cars_api import models
+from cars_api.models import *
 from random import randint
 
 
 @pytest.mark.django_db
 def test_add_car(client, set_up_cars):
-    set_up_count = models.Car.objects.count()
+    set_up_count = Car.objects.count()
     a = {
-        'make': 'Ford',
-        'model': 'Focus'
+        'make': 'Suzuki',
+        'model': 'Swift'
     }
     response = client.post('/cars/', data=a, format='json')
     assert response.status_code == 201
-    assert models.Car.objects.count() == set_up_count + 1
+    assert Car.objects.count() == set_up_count + 1
     for key, value in a.items():
         assert key in response.data
         assert response.data[key] == value
@@ -32,12 +32,12 @@ def test_add_not_existing_car(client):
 def test_get_cars_list(client, set_up_cars):
     response = client.get('/cars/', {}, format='json')
     assert response.status_code == 200
-    assert models.Car.objects.count() == len(response.data)
+    assert Car.objects.count() == len(response.data)
 
 
 @pytest.mark.django_db
 def test_get_car_detail(client, set_up_cars):
-    car = models.Car.objects.first()
+    car = Car.objects.first()
     response = client.get(f'/cars/{car.id}/', {}, format='json')
     assert response.status_code == 200
     for field in ('id', 'make', 'model', 'avg_rating'):
@@ -46,29 +46,31 @@ def test_get_car_detail(client, set_up_cars):
 
 @pytest.mark.django_db
 def test_delete_car(client, set_up_cars):
-    car = models.Car.objects.first()
+    car = Car.objects.first()
     response = client.delete(f'/cars/{car.id}/', {}, format='json')
     assert response.status_code == 204
-    car_ids = [car.id for car in models.Car.objects.all()]
+    car_ids = [car.id for car in Car.objects.all()]
     assert car.id not in car_ids
 
 
 @pytest.mark.django_db
 def test_update_car(client, set_up_cars):
-    car = models.Car.objects.first()
+    car = Car.objects.first()
     response = client.get(f'/cars/{car.id}/', {}, format='json')
     car_data = response.data
-    new_model = 'Fit'
+    new_make = 'Fiat'
+    new_model = '500'
+    car_data['make'] = new_make
     car_data['model'] = new_model
     response = client.patch(f'/cars/{car.id}/', car_data, format='json')
     assert response.status_code == 200
-    car_obj = models.Car.objects.get(id=car.id)
+    car_obj = Car.objects.get(id=car.id)
     assert car_obj.model == new_model
 
 
 @pytest.mark.django_db
 def test_update_to_non_existing_car(client, set_up_cars):
-    car = models.Car.objects.first()
+    car = Car.objects.first()
     response = client.get(f'/cars/{car.id}/', {}, format='json')
     car_data = response.data
     new_model = 'Nonexistent'
@@ -79,7 +81,7 @@ def test_update_to_non_existing_car(client, set_up_cars):
 
 @pytest.mark.django_db
 def test_add_car_rate(client, set_up_cars):
-    car = models.Car.objects.first()
+    car = Car.objects.first()
     a = {
         'car_id': car.id,
         'rating': randint(1, 5)
@@ -103,12 +105,12 @@ def test_add_not_existing_car_rate(client):
 def test_get_popular_cars_list(client, set_up_cars):
     response = client.get('/popular/', {}, format='json')
     assert response.status_code == 200
-    assert models.Car.objects.count() == len(response.data)
+    assert Car.objects.count() == len(response.data)
 
 
 @pytest.mark.django_db
 def test_get_popular_cars_list_ordering(client, set_up_cars, set_up_car_rate):
-    car = models.Car.objects.get(id=set_up_car_rate.car_id.id)
+    car = Car.objects.get(id=set_up_car_rate.car_id.id)
     response = client.get('/popular/', {}, format='json')
     assert response.status_code == 200
     assert car.id == response.data[0]['id']
